4311aa9166960c5d237a00f0092ed5d866b51e32
SthephanySouza030 <sthephany.souza@conectainfra.digital>
2026-08-03 15:13:38 -0300

    update projetos

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,30 @@
+import styles from './cards.module.css'
+import { FaHtml5, FaCss3Alt, FaJs, FaReact } from 'react-icons/fa'
+import { BsArrowRight } from 'react-icons/bs'
+
+function Cards() {
+    return (
+        <section className={styles.card}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <div className={styles.card_footer}>
+                <div className={styles.card_icones}>
+                    <FaHtml5 />
+                    <FaCss3Alt />
+                    <FaJs />
+                    <FaReact />
+                </div>
+
+                <a
+                    href={html_url}
+                    className={styles.botao}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <BsArrowRight />
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default Cards
--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,18 +1,45 @@
 import Container from "../../components/Container"
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
+import Cards from "../../components/Cards"
+
+import { useEffect, useState } from "react"
+
+import styles from './projetos.module.css'
 
 const Projetos = () => {
+    const [ repositories, setRepositories ] = useState ([])
+
+    useEffect(() => {
+        const buscarRepositorios = async() => {
+            const response = await fetch('')
+            const data = await response.json()
+        }
+        buscarRepositorios()
+    }, [])
+
     return (
-        <>
-            <Header />
-
-            <Container>
-                <h1>Projetos</h1>
-            </Container>
-
-            <Footer />
-        </>
+        <section className={styles.projetos}>
+            <h2>Projetos</h2>
+            {
+                repositories.length > 0 ? (
+                    <section className={styles.lista}>
+                        {
+                            repositories.map((repo) => {
+                                <Cards
+                                    key={repo.id}
+                                    name={repo.name}
+                                    description={repo.description}
+                                    html_url={repo.html_url}
+                                />
+                            })
+                        }
+                    </section>
+                ) : (
+                    <p>Carregando Projetos...</p>
+                )
+            }
+        </section>
     )
 }
 
